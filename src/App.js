aeae1bc2a17e51723fdf2324eb53b0c04e663fb5
Rajal Kumar <rajalkumar13@hotmail.com>
2019-10-14 00:02:46 +1300

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is a starting point for my project of the day starting on the 14th of October 2019 :)</h1>
     </div>
   );
 }
