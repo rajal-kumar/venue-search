7f8b5c30af212ce5d9be0c958e18c897050f3ef9
Rajal Kumar <rajalkumar13@hotmail.com>
2019-10-14 17:43:48 +1300

    added superagent to make https requests for the API

--- src/components/Venues.jsx
+++ src/components/Venues.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 class Venues extends React.Component {
   render() {
     return (
-      <div>Venues Component</div>
+      <div>
+        <ol>
+          <li>Venue 1</li>
+          <li>Venue 2</li>
+          <li>Venue 3</li>
+          <li>Venue 4</li>
+        </ol>
+      </div>
     )
   }
 }
