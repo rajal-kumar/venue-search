8da47d32aad97b7ba6398d086bf6fabef7922cda
Rajal Kumar <rajalkumar13@hotmail.com>
2019-10-14 16:47:39 +1300

    added input and button fields also added state to the component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
 class Nav extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      zipCode: ''
+    }
+  }
+  
+  searchVenue() {
+    console.log('searchVenues: ' + this.state.zipCode)
+  }
+
+  updateZipCode(event) {
+    console.log('updateZipCode: ' + event.target.value)
+
+    //This is updating the values in the state.
+    this.setState({
+      zipCode: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        Nav Component
+        <input onChange={this.updateZipCode.bind(this)} type="text" placeholder="Enter Zip code" />
+        <button onClick={this.searchVenue.bind(this)}>Search</button>
       </div>
     )
   }
